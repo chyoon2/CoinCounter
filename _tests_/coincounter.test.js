a33ee861b0224da8878b5ae30b5c6825b3a2a917
Frederick Ernest <frederick.ernest@gmail.com>
2020-08-31 15:39:16 -0700

    try multiple closures
    Co-authored-by: Chris Yoon <chy.yoon@gmail.com>

--- _tests_/coincounter.test.js
+++ _tests_/coincounter.test.js
@@ -1,41 +1,46 @@
-import {incrementCounter} from './../src/coincounter.js';
+import {incrementCounter, numberOfNickels} from './../src/coincounter.js';
+import {numberOfQuarters, numberOfDimes, numberOfPennies} from './../src/coincounter.js';
 
 test('should return string of coin amounts', () => {
   let amount = 4.99;
-   expect(incrementCounter(amount)).toEqual("4 Dimes:2 Quarters: 19");
+  expect(incrementCounter(amount)).toEqual("Pennies: 4 Dimes: 2 Quarters: 19");
 });
 
+test('should return coin amounts for dollar inputs', () => {
+  let amount = 4.99;
+  expect(numberOfQuarters(amount)).toEqual(19);
+});
 
+test('test for array values', () => {
+  let amount = 2342.99
+  let arr = [numberOfQuarters(amount), numberOfDimes(amount), numberOfPennies(amount), numberOfNickels(amount)]
+  expect(arr[0]).toEqual(9371);
+  expect(arr[1]).toEqual(23429);
+  expect(arr[2]).toEqual(234298);
+  expect(arr[3]).toEqual(46859);
+});
 
-  // //quarters dimes nickles pennies
-  // let divider = (number) =>{
-  //   if ((number % .25) === 0){
-  //     number / .25
-  //   }
-  // };
-//   function amountTocoins(amount, coins) 
-// {
-//   if (amount === 0) 
-//   {
-//     return [];
-//   } 
-//   else
-//     {
-//       if (amount >= coins[0]) 
-//         {
-//         left = (amount - coins[0]);
-//         return [coins[0]].concat( amountTocoins(left, coins) );
-//         } 
-//       else
-//         {
-//           coins.shift();
-//           return amountTocoins(amount, coins);
-//         }
-//     }
-// } 
-// console.log(amountTocoins(1239, [25, 10, 5, 1]));
-//   expect(answer).toEqual(2);
-// });
+test('new tests', () => {
+  function neww() {
+    return function(Quarters) {
+      return `Quaters: ${Quarters / 2}`
+      
+    return function(Dimes) {
+      return `Dimes: ${Quarters}`
+    }
+      // return function(Dimes) {
+      //   return function(Nickels) {
+      //     return function(Pennies) {
+      //       return `Quaters: ${Quarters}, Dimes: ${Dimes}, Nickels: ${Nickels}, Pennies: ${Pennies}!`
+      //     }
+      //   }
+      // }
+    }
+  }
+  const result = neww();
+  expect(result("5")).toEqual("");
+});
 
-
-
+// expect(result("4")).toEqual("");
+// expect(result("3")).toEqual("");
+    // expect(result("2")).toEqual("");
